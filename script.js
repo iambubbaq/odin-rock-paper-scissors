66a9612e41214362b044d72acfadcf2e23d8123e
Stephen Williams <25148210+iambubbaq@users.noreply.github.com>
2023-07-18 12:10:34 -0600

    Add link to script and prepare pseudocode

---
+++ script.js
@@ -0,0 +1,5 @@
+//getComputerChoice to randomly select Rock, Paper, Or Scissors
+//create playRound function with two parameters of computerSelection and playerSelection
+//prompt playerChoice and store as case insensitive
+//return result as a string with choice beating choice
+//game funtion to loop for 5 rounds
