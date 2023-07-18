fed664a1a7696dfa124931178f661ad358e576ef
Stephen Williams <25148210+iambubbaq@users.noreply.github.com>
2023-07-18 12:27:27 -0600

    Add getComputerChoice function

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 //getComputerChoice to randomly select Rock, Paper, Or Scissors
+function getComputerChoice() {
+    const choices = ["rock", "paper", "scissors"];
+    let computerSelection = choices[Math.floor(Math.random() * choices.length)];
+    return computerSelection;
+}
 //create playRound function with two parameters of computerSelection and playerSelection
 //prompt playerChoice and store as case insensitive
 //return result as a string with choice beating choice
